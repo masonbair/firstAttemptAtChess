9dee88cbcaceec9a5a9dc3be209b931763669d7a
SaltiestCarrot <mason.s.bair@gmail.com>
2023-07-11 11:27:41 -0400

    Update pieces.java

--- pieces.java
+++ pieces.java
@@ -13,55 +13,55 @@ public abstract class pieces {
     public pieces(String name, boolean clor){ // true is black, false is white
         if(name.equals("Pawn")){
             if(clor) {
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/Black_pawn.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/Black_pawn.png";
                 color = "black";
             }else{
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/White_pawn.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/White_pawn.png";
                 color = "white";
             }
             pieceCode = 1;
         }else if(name.equals("Rook")){
             if(clor) {
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/Black_rook.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/Black_rook.png";
                 color = "black";
             }else{
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/White_rook.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/White_rook.png";
                 color = "white";
             }
             pieceCode = 2;
         }else if(name.equals("Knight")){
             if(clor) {
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/Black_knight.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/Black_knight.png";
                 color = "black";
             }else{
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/White_knight.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/White_knight.png";
                 color = "white";
             }
             pieceCode = 3;
         }else if(name.equals("Bishop")){
             if(clor) {
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/Black_bishop.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/Black_bishop.png";
                 color = "black";
             }else{
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/White_bishop.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/White_bishop.png";
                 color = "white";
             }
             pieceCode = 4;
         }else if(name.equals("Queen")){
             if(clor) {
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/Black_queen.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/Black_queen.png";
                 color = "black";
             }else{
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/White_queen.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/White_queen.png";
                 color = "white";
             }
             pieceCode = 5;
         }else if(name.equals("King")){
             if(clor) {
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/Black_king.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/Black_king.png";
                 color = "black";
             }else{
-                I = "/home/mason/IdeaProjects/Chess/src/Pieces/White_king.png";
+                I = "/home/mason/IdeaProjects/Chess/src/firstAttemptAtChess/Pieces/White_king.png";
                 color = "white";
             }
             pieceCode = 6;
